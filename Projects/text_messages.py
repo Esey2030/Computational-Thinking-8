9d6d36255cd4878f54ce96ad31a07687f793c066
Esey2030 <eseyberhane@seattleacademy.org>
2025-09-10 18:28:00 +0000

    hi josh

---
+++ Projects/text_messages.py
@@ -0,0 +1,49 @@
+num1 = input("Enter first Number: ")
+num2 = input("Enter second Number: ")
+
+print(num1 + num2)
+
+
+team = input("what is your favorite basketball team? ") 
+point_guard = input("who is the point guard of "+ team + "? ")
+print(f"the point guard of the {team} is {point_guard}")
+shooting_guard = input("who is the shooting guard of "+ team + "? ")
+print(f"the shooting guard of the {team} is {shooting_guard}")
+small_forward = input("who is the small forward of "+ team + "? ")
+print(f"the small forward of the {team} is {small_forward}")
+power_forward = input("who is the power forwrad of "+ team + "? ")
+print(f"the power forward of the {team} is {power_forward}")
+center = input("who is the center of "+ team + "? ")
+print(f"the center of the {team} is {center}")
+state = input("where is the state of the "+ team + "? ")
+print(f"the state of the {team} is {state}")
+coach = input("who is the coach of the "+ team + "? ")
+print(f"the coach of the {team} is coach {coach}")
+print("Thank you for playing")
+
+
+
+
+
+
+
+
+print("name of a relative ")
+relative = input("")
+print("describing word ")
+describing_word = input("")
+print("Something you would find in the woods ")
+something_in_woods = input("")
+print("thing or animal")
+thing_animal = input("thing or animal")
+print("thing or animal")
+thing_animal2 = input("thing or animal")
+thing_animal3 = input("thing or animal")
+action = input("action word")
+action2 = input("action word")
+describing_word2 = input("describing word")
+place = input("place")
+thing = input("thing")
+action3 = input("action word")
+relationship = input("relastionship to someone")
+print(f"There once was a fox. His name was {relative}. This {describing_word} creature was full of curiiosity. {relative} liked to go hunting for {something_in_woods} in the woods. His favorite {thing_animal} was a {thing_animal2}. {relative} picked up the {thing_animal3} and {action} from it before {action2} back to the {describing_word2} {place} with the {thing} he {action3} for his {relationship} ")
