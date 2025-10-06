70c07b895c88dfed49e7c43fb85b6b04f5a0a234
Esey2030 <eseyberhane@seattleacademy.org>
2025-10-06 19:02:11 +0000

    done

--- Projects/project2.py
+++ Projects/project2.py
@@ -22,5 +22,29 @@ if answer == "A":
 elif answer == "B":
 	basketball_points += 1
 
+answer = input("Are you better A)with your feet B) or hands?")
+if answer == "A":
+	soccer_points += 1
+elif answer == "B":
+	basketball_points += 1
+
+answer = input("Would you rather A)wear grip socks or B) normal socks?")
+if answer == "A":
+	soccer_points += 1
+elif answer == "B":
+	basketball_points += 1
+
+
 print(f"basketball points = {basketball_points}")
 print(f"soccer points = {soccer_points}")
+
+if soccer_points > basketball_points:
+	print("youre a soccer player")
+elif soccer_points < basketball_points:
+	print("youre a basketball player")
+
+if answer == "b"and basketball_points:
+	print("you really like basketball")
+
+if answer == "a" or answer == "b":
+	soccer_points+= 1
