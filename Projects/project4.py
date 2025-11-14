5e8568ce16b16ccee1f3aeffb45c107b65afd3e6
Esey2030 <eseyberhane@seattleacademy.org>
2025-11-14 21:40:35 +0000

    done

--- Projects/project4.py
+++ Projects/project4.py
@@ -1,15 +1,18 @@
-
+#makes the turtle
 import turtle
 
 
 t = turtle.Turtle()
 t.speed(10)
+#stops drawing
 t.penup()
 t.goto(-150,-150)
 t.color("purple")
 t.pendown()
+#Gives variation of colors
 colors = ["red", "blue", "green"]
 for i in range(10):
+#Made the colors random
     t.color(colors[i % 3] )
     t.circle(30)
     t.forward(67)
